9f99d6893854311b79590b639087d7b57525ca64
Bruno de Farias <farias.brunof@gmail.com>
2024-03-18 17:59:26 -0300

    commit inicial

---
+++ src/views/QuizView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Question from "../components/Question.vue";
+import QuizHeader from "../components/QuizHeader.vue";
+import { useRoute } from "vue-router";
+import { ref, watch, computed } from "vue";
+import quizes from "../data/data.json";
+
+const route = useRoute();
+const quizId = parseInt(route.params.id);
+const quiz = quizes.find(q => q.id === quizId);
+const currentQuestionIndex = ref(0);
+
+// const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`);
+
+// watch(
+//   () => currentQuestionIndex.value,
+//   () => {
+//     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+//   }
+// );
+const questionStatus = computed(() => {
+  return `${currentQuestionIndex.value}/${quiz.questions.length}`;
+});
+const barPercentage = computed(() => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`);
+</script>
+<template>
+  <div>
+    <QuizHeader :questionStatus="questionStatus" :barPercentage="barPercentage" />
+    <div>
+      <Question :question="quiz.questions[currentQuestionIndex]" />
+    </div>
+    <button @click="currentQuestionIndex++">Next question</button>
+  </div>
+</template>
+
+<style scoped></style>
