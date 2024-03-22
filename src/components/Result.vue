e54e45e18fdd7e0d7fd49478de2a64a3346d4bea
Bruno Fernando de Farias <farias.brunof@gmail.com>
2024-03-22 19:22:20 -0300

    Versao final

---
+++ src/components/Result.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps } from "vue";
+const { numberOfCorrectAnswers, quizQuestionLength } = defineProps([
+  "numberOfCorrectAnswers",
+  "quizQuestionLength",
+]);
+</script>
+<template>
+  <div class="results"></div>
+  <p>Your results...</p>
+  <h1>{{ numberOfCorrectAnswers }}|{{ quizQuestionLength }}</h1>
+</template>
+
+<style scoped>
+.results {
+  text-align: center;
+  padding: 100px 0;
+}
+p {
+  font-size: 25px;
+}
+
+h1 {
+  font-size: 80px;
+}
+</style>
